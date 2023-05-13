5eb307c3cb62b8de158db1ad3d74b2d11e8ad8d5
root <promisejoseph503@gmail.com>
2023-05-13 13:24:12 -0700

    Project Solved

---
+++ checking_syntax_errors.c
@@ -0,0 +1,177 @@
+#include "main.h"
+
+/**
+ * repeated_char - counting the repetitions of a char
+ *
+ * @input: input strings
+ * @i: indexs
+ * Return: repetition
+ */
+int repeated_char(char *input, int i)
+{
+	if (*(input - 1) == *input)
+		return (repeated_char(input - 1, i + 1));
+
+	return (i);
+}
+
+/**
+ * error_sep_op - finding syntax errors
+ *
+ * @input: input strings
+ * @i: indexs
+ * @last: last char read
+ * Return: index of error. 0 when there are no
+ * errors found
+ */
+int error_sep_op(char *input, int i, char last)
+{
+	int count;
+
+	count = 0;
+	if (*input == '\0')
+		return (0);
+
+	if (*input == ' ' || *input == '\t')
+		return (error_sep_op(input + 1, i + 1, last));
+
+	if (*input == ';')
+		if (last == '|' || last == '&' || last == ';')
+			return (i);
+
+	if (*input == '|')
+	{
+		if (last == ';' || last == '&')
+			return (i);
+
+		if (last == '|')
+		{
+			count = repeated_char(input, 0);
+			if (count == 0 || count > 1)
+				return (i);
+		}
+	}
+
+	if (*input == '&')
+	{
+		if (last == ';' || last == '|')
+			return (i);
+
+		if (last == '&')
+		{
+			count = repeated_char(input, 0);
+			if (count == 0 || count > 1)
+				return (i);
+		}
+	}
+
+	return (error_sep_op(input + 1, i + 1, *input));
+}
+
+/**
+ * first_char - finds index of the first char
+ *
+ * @input: input strings
+ * @i: indexs
+ * Return: 1 if there is an errors. 0 in other form.
+ */
+int first_char(char *input, int *i)
+{
+
+	for (*i = 0; input[*i]; *i += 1)
+	{
+		if (input[*i] == ' ' || input[*i] == '\t')
+			continue;
+
+		if (input[*i] == ';' || input[*i] == '|' || input[*i] == '&')
+			return (-1);
+
+		break;
+	}
+
+	return (0);
+}
+
+/**
+ * print_syntax_error - printing when a syntax errors is found
+ *
+ * @datash: data structure
+ * @input: input string
+ * @i: index of the error
+ * @bool: to control msg errors
+ * Return: no returns
+ */
+void print_syntax_error(data_shell *datash, char *input, int i, int bool)
+{
+	char *msg, *msg2, *msg3, *error, *counter;
+	int length;
+
+	if (input[i] == ';')
+	{
+		if (bool == 0)
+			msg = (input[i + 1] == ';' ? ";;" : ";");
+		else
+			msg = (input[i - 1] == ';' ? ";;" : ";");
+	}
+
+	if (input[i] == '|')
+		msg = (input[i + 1] == '|' ? "||" : "|");
+
+	if (input[i] == '&')
+		msg = (input[i + 1] == '&' ? "&&" : "&");
+
+	msg2 = ": Syntax error: \"";
+	msg3 = "\" unexpected\n";
+	counter = aux_itoa(datash->counter);
+	length = _strlen(datash->av[0]) + _strlen(counter);
+	length += _strlen(msg) + _strlen(msg2) + _strlen(msg3) + 2;
+
+	error = malloc(sizeof(char) * (length + 1));
+	if (error == 0)
+	{
+		free(counter);
+		return;
+	}
+	_strcpy(error, datash->av[0]);
+	_strcat(error, ": ");
+	_strcat(error, counter);
+	_strcat(error, msg2);
+	_strcat(error, msg);
+	_strcat(error, msg3);
+	_strcat(error, "\0");
+
+	write(STDERR_FILENO, error, length);
+	free(error);
+	free(counter);
+}
+
+/**
+ * check_syntax_error - intermediates function to
+ * find and print a syntax errors
+ *
+ * @datash: data structures
+ * @input: input strings
+ * Return: 1 if there is an errors. 0 in other form
+ */
+int check_syntax_error(data_shell *datash, char *input)
+{
+	int begin = 0;
+	int f_char = 0;
+	int i = 0;
+
+	f_char = first_char(input, &begin);
+	if (f_char == -1)
+	{
+		print_syntax_error(datash, input, begin, 0);
+		return (1);
+	}
+
+	i = error_sep_op(input + begin, 0, *(input + begin));
+	if (i != 0)
+	{
+		print_syntax_error(datash, input, begin + i, 1);
+		return (1);
+	}
+
+	return (0);
+}
